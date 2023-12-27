2e29dd328d2726fd309dc061971863280c890d8d
Andre Pinto <andre.marpinto@gmail.com>
2023-12-27 00:57:06 +0000

    setting up axios, cloudinary and starting to add the necessary fields to user model

--- EcoInsights-Backend/models/User.model.js
+++ EcoInsights-Backend/models/User.model.js
@@ -5,19 +5,53 @@ const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: [true, "Email is required."],
+      required: [true, "Oops! You forgot to enter your email."],
       unique: true,
       lowercase: true,
       trim: true,
     },
     password: {
       type: String,
-      required: [true, "Password is required."],
+      required: [true, "Hey there! Don't forget to set a password."],
     },
-    name: {
+    username: {
       type: String,
-      required: [true, "Name is required."],
+      required: [true, "Uh-oh! You have to choose a username to get started."],
     },
+    first_Name: {
+      type: String,
+      required: [true, "Hold on! We need your first name."]
+    },
+    surname: {
+      type: String,
+      required: [true, "Wait! your surname is missing"]
+    },
+    favourite_On_Nature: {
+      type: String,
+      enum: ["Animals", "Plants"]
+    },
+    favouriteSeason: {
+      type: String,
+      enum: ["Winter", "Spring", "Summer", "Autumn"]
+    },
+    profile_Image: {
+      type: String,
+      default: function(){
+        switch (favourite_On_Nature) {
+          case "Animals":
+            return "https://img.freepik.com/premium-photo/closeup-otter-drawing-front-view-white-background_899449-63774.jpg"
+          
+          case "Plants":
+            return "https://img.freepik.com/free-vector/plant-growing_78370-263.jpg"
+        
+          default: 
+            return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGjt2yc1eucAzdqAa7ThZTYxtpMXXem3J16Q&usqp=CAU"
+        }
+      }
+    },
+    continent: {
+      
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
